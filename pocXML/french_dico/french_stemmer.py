fbf062d87620e48fe1a83763237e0d4bfe287227
ouassimBenMosbah <wassimbenmosbah@gmail.com>
2017-12-08 11:49:57 +0100

    dico improved

--- pocXML/french_dico/french_stemmer.py
+++ pocXML/french_dico/french_stemmer.py
@@ -1,4 +1,5 @@
-""" Transform the french dictionary into stemmed words """
+""" Transform the french dictionary into phonetic stemmed words """
+import os
 from nltk.stem import SnowballStemmer
 stemmer = SnowballStemmer("french")
 
@@ -10,4 +11,20 @@ with open('french.txt', 'r') as f:
 
 # french_stem.txt is the new dictionary
 with open('french_stem.txt', 'w') as f:
-    f.write("\n".join(sorted(dict_root)))
+    f.write("\n\n".join(sorted(dict_root)))
+
+os.system('espeak -v fr -f french_stem.txt -x -q > french_stem_pho.txt')
+
+french_stem_pho = []
+with open('french_stem_pho.txt', 'r') as f:
+    for line in f:
+        french_stem_pho += line.split(' ')
+
+french_stem_pho_res = []
+print(french_stem_pho[:25])
+for x in french_stem_pho:
+    if x.strip('\n '):
+        french_stem_pho_res.append(x.strip('\n '))
+
+with open('french_stem_pho.txt', 'w') as f:
+    f.write("\n".join(french_stem_pho_res))
