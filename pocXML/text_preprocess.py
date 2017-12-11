6ab8831f10a7e160599414cf7a143f55621a41ea
ouassimBenMosbah <wassimbenmosbah@gmail.com>
2017-12-11 14:26:35 +0100

    bug fix

--- pocXML/text_preprocess.py
+++ pocXML/text_preprocess.py
@@ -11,29 +11,30 @@ import french_dico.french_stem_pho
 
 dico_fr = french_dico.french.french.split("\n")
 dict_pho_fr = dict()
+dico_sms_fr = dict()
 
 for french_pho_word, french_word in zip(french_dico.french_pho.french_pho.split("\n"), dico_fr):
     dict_pho_fr.update({french_pho_word: french_word})
-
-dico_sms_fr = dict()
 
 for french_sms_word, french_sms_trad in zip(sms_dico.sms.sms_dico.split("\n"), sms_dico.sms_traduction.sms_dico_trad.split("\n")):
     dico_sms_fr.update({french_sms_word: french_sms_trad})
 
 def preprocess_sms(list_sms):
     clean_sms = []
-    random_index = randrange(0,len(list_sms))
-    for sms in list_sms[random_index:random_index+1]:
-        words = word_tokenize(sms, language='french')
-        for word in words:
-            if word in dico_fr:
-                print(word)
-            elif word in dico_sms_fr:
-                print(dico_sms_fr[word])
-            else:
-                word_pho = check_output(['espeak', '-v', 'fr', '-x', '-q', word]).decode('utf8').strip()
-                if word_pho in dict_pho_fr:
-                    print(dict_pho_fr[word_pho])
-                    pass
-        break
-    return list_sms
+    for sms in list_sms[:300]:
+        try:
+            words = word_tokenize(sms, language='french')
+            for word in words:
+                if word in dico_fr:
+                    word_to_add = word
+                elif word in dico_sms_fr:
+                    word_to_add = dico_sms_fr[word]
+                else:
+                    word_pho = check_output(['espeak', '-v', 'fr', '-x', '-q', '"'+word+'"']).decode('utf8').strip()
+                    if word_pho in dict_pho_fr:
+                        word_to_add = dict_pho_fr[word_pho]
+        except:
+            word_to_add = ""
+        finally:
+            clean_sms.append(word_to_add)
+    return clean_sms
