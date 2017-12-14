3e61918a93c4592ae4b7cb6ece381271edbb6010
clementbouly <clementbouly@hotmail.fr>
2017-12-14 16:25:57 +0100

    pre process xml correction

--- main/data_getter.py
+++ main/data_getter.py
@@ -3,17 +3,35 @@ import os
 from collections import Counter
 from lxml import etree
 import re
+import time
 
 # Name of the file containing the messages
 fname = os.path.join(os.path.dirname(__file__), os.pardir, "sms.xml")
 
-# with open(fname, 'r') as f:
-#     corpus = f.read().replace('<3', '(coeur)')
-# corpus_modified = re.sub(r"<cont>(?P<a>.*)<(?P<b>.*)</cont>", r"<cont>\g<a> \g<b></cont>", corpus)
+start = time.time()
 
-# with open(fname, 'w') as f:
-#     f.write(corpus_modified)
-# tree = etree.parse(fname)
+res = ""
+with open(fname, 'r') as f:
+    corpus = f.read().split("</sms>")
+
+
+for sms in corpus:
+    sms = sms.replace("\n", "").replace('<3', '(coeur)').replace('&', 'et')
+    match = re.search(r"<cont>.*<.*</cont>", sms)
+    if match:
+        sms = re.sub(r"<cont>(?P<a>.*)</cont>", "<cont>" + match.group()[6:-7].replace('<', ' ') + "</cont>", sms)
+    res += sms + "</sms>\n"
+
+with open(fname, 'w') as f:
+    f.write(res[:-7])
+
+start = time.time() - start
+print("Execution time for REGEX :", round(start, 2), "seconds")
+
+tree = etree.parse(fname)
+
+
+
 
 # Split the whole document sms content by sms content
 sms_bodies = [sms.text for sms in tree.xpath("/corpus/sms/cont")]
