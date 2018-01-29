0d98bd2c2d11f96a1464a511f424d75a6ad49c59
ouassimBenMosbah <wassimbenmosbah@gmail.com>
2018-01-29 13:57:07 +0100

    Refactoring: Code cleaning

--- main/reg_exps.py
+++ main/reg_exps.py
@@ -3,15 +3,15 @@
     used to search for specific data in text
 '''
 # List of dates
-dates = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi",
-         "dimanche", "janvier", "fevrier", "mars", "avril", "mai",
-         "juin", "juillet", "aout", "septembre", "octobre", "novembre",
-         "décembre", "demain", "aujoud'hui", "hier", "midi", "minuit", "soir"]
+dates = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi',
+         'dimanche', 'janvier', 'fevrier', 'février', 'mars', 'avril', 'mai',
+         'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre',
+         'décembre', 'demain', "aujoud'hui", 'hier', 'midi', 'minuit', 'soir']
 
 # List of regexp
-regexp_hours = r"\b[0-9]{1,2}( )*[h:]( )*[0-9]{0,2}\b"
-regexp_money = r"""\b[0-9]+( )*(euros|e|eur|euro|dollar|dollars|
-                    boul|patate|boule|boules)\b"""
-regexp_currency = r"(€|\$)"
-regexp_email = r"\b[a-z]*@[a-z]*[.]\b"
-regexp_tel = r"(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}"
+regexp_hours = r'\b[0-9]{1,2}( )*[h:]( )*[0-9]{0,2}\b'
+regexp_money = r'''\b[0-9]+( )*(euros|e|eur|euro|dollar|dollars|
+                    boul|patate|boule|boules)\b'''
+regexp_currency = r'(€|\$)'
+regexp_email = r'\b[a-z]*@[a-z]*[.]\b'
+regexp_tel = r'(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}'
