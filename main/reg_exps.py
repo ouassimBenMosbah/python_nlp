eadf6b0eb4582ad04109c475a7b3485006407a17
ouassimBenMosbah <wassimbenmosbah@gmail.com>
2018-01-29 17:12:09 +0100

    test: add new tests

--- main/reg_exps.py
+++ main/reg_exps.py
@@ -6,12 +6,13 @@
 dates = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi',
          'dimanche', 'janvier', 'fevrier', 'février', 'mars', 'avril', 'mai',
          'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre',
-         'décembre', 'demain', "aujoud'hui", 'hier', 'midi', 'minuit', 'soir']
+         'decembre', 'décembre', 'demain', "aujoud'hui", 'hier', 'midi',
+         'minuit', 'soir']
 
 # List of regexp
 regexp_hours = r'\b[0-9]{1,2}( )*[h:]( )*[0-9]{0,2}\b'
 regexp_money = r'''\b[0-9]+( )*(euros|e|eur|euro|dollar|dollars|
-                    boul|patate|boule|boules)\b'''
+                    boul|patate|patates|boule|boules)\b'''
 regexp_currency = r'(€|\$)'
 regexp_email = r'\b[a-z]*@[a-z]*[.]\b'
 regexp_tel = r'(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}'
--- tests.py
+++ tests.py
@@ -1,12 +1,62 @@
 import unittest
+import os
+import re
+import main.reg_exps as regexs
 
-class TestMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+class TestMethods(unittest.TestCase): 
+    def setUp(self):
+        self.dates = regexs.dates
+        self.list_regexs = [
+            regexs.regexp_hours,
+            regexs.regexp_money,
+            regexs.regexp_currency,
+            regexs.regexp_email,
+            regexs.regexp_tel
+        ]
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_regexs(self):
+        list_sentences = [
+            'Il faudrait qu\'on se donne rendez-vous demain',
+            'Elle est morte mardi?c\'est ca?',
+            'resto a 20h!!',
+            'ça m\'a couté 19,99€ lol',
+            'il me doit 100 boules',
+            'il va me passé10patates!!'
+            'test@yopmail.com',
+            'test2@société.io'
+            '06.12.34.56.78',
+            '0612345678',
+            '07 12 34 56 78',
+            '06 12 34 5678',
+            '+336 12345678'
+            'son num é0612345678.'
+        ]
+
+        for sentence in list_sentences:
+            self.assertTrue(
+                any(re.search(regex, sentence) for regex in self.list_regexs) or any(
+                    re.search(r'\b' + date + '\\b', sentence) for date in self.dates))
+    
+    def test_regexs_not_matching(self):
+        list_sentences = [
+            'Il faudrait qu\'on se voit',
+            'il a fait beau',
+            'on va manger au resto !',
+            'il a 1988 idées',
+            'test@com',
+            'ce sont ces patates!',
+            '06.12.34.56.8',
+            '061234567',
+            '06 12 34 6 78',
+            '0012345678',
+            '+216 12 34 56 78'
+        ]
+
+        for sentence in list_sentences:
+            self.assertFalse(
+                any(re.search(regex, sentence) for regex in self.list_regexs) or any(
+                    re.search(r'\b' + date + '\\b', sentence) for date in self.dates))
+
 
 if __name__ == '__main__':
     unittest.main()
